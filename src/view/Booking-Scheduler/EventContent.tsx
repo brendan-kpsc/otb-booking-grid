e0c8bedb6d0ca43bf207b54f147ece24c2980f06
Brendan Caldwell <brendan.caldwell@abstractive.ca>
2025-08-30 23:04:22 -0700

    updated tooltip with the correct values

--- src/view/Booking-Scheduler/EventContent.tsx
+++ src/view/Booking-Scheduler/EventContent.tsx
@@ -14,16 +14,14 @@ const CustomWidthTooltip = styled(({ className, ...props }: TooltipProps) => (
 export const EventContent =  (arg: any) => {
     const refNumber = arg.event.extendedProps.refNumber;
     const contactName = arg.event.extendedProps.contactName;
-    const businessPhoneNumber = arg.event.extendedProps.businessPhoneNumber?.replace(/(\d{3})(\d{3})(\d{3})/, '$1-$2-$3');
     const homePhoneNumber = arg.event.extendedProps.homePhoneNumber?.replace(/(\d{3})(\d{3})(\d{3})/, '$1-$2-$3');
-    const companyPhoneNumber = arg.event.extendedProps.homePhoneNumber?.replace(/(\d{3})(\d{3})(\d{3})/, '$1-$2-$3');
+    const mobilePhoneNumber = arg.event.extendedProps.mobilePhoneNumber?.replace(/(\d{3})(\d{3})(\d{3})/, '$1-$2-$3');
 
     const label = <>
         <Typography variant="body2"><strong>Ref #:</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{refNumber}</Typography>
         <Typography variant="body2"><strong>Contact:</strong>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{contactName}</Typography>
-        <Typography variant="body2"><strong>Phone (B):</strong>&nbsp;&nbsp;{businessPhoneNumber}</Typography>
         <Typography variant="body2"><strong>Phone (H):</strong>&nbsp;&nbsp;{homePhoneNumber}</Typography>
-        <Typography variant="body2"><strong>Phone (C):</strong>&nbsp;&nbsp;{companyPhoneNumber}</Typography>
+        <Typography variant="body2"><strong>Phone (M):</strong>&nbsp;&nbsp;{mobilePhoneNumber}</Typography>
     </>
 
     return (
